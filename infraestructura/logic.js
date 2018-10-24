0ed1daab517d33ea90f0b4dcdd4d1d1e665f5477
brm-valenciad <42819769+brm-valenciad@users.noreply.github.com>
2018-10-24 11:40:33 -0500

    Update logic.js

--- infraestructura/logic.js
+++ infraestructura/logic.js
@@ -8,3 +8,38 @@ console.error("Nos encargamos de hacer la logica");
         });
         
      $('#start-date').val(new Date().toDateInputValue()).attr("min", new Date().toDateInputValue());
+
+console.warn("Formateamos las fechas");
+
+//Cargando Puestos de trabajo
+        loadInfo("21/PUESTO/grupo/false/", "jobPlace");
+        //Cargando puestos de trabajo
+        loadInfo("21/EQUIPO%20DE%20COMPUTO/grupo/false/", "computerEquipment");
+        //Cargando licencias de equipos
+        loadInfo("21/LICENCIA%20EQUIPO%20DE%20COMPUTO/grupo/false/", "computerLicences");
+        //Cargando Diademas
+        loadInfo("21/DIADEMA/grupo/false/","headbands");
+        //Cargando Marcadores
+        loadInfo("21/LICENCIA%20DE%20MARCADORAS/grupo/false/","lincencesMarkers");
+        //Cargar Adicionales
+        loadInfo("21/ADICIONALES/grupo/false/","additionalsElements");
+
+        //Carga
+        function loadInfo(table, place){
+            var url_ = "https://servon.com.co/index/dataGet/be5c205a1cec81934cee5a7e6e28f34e/1/1000/";
+                url_ += table;
+                 $.getJSON(
+                    url_,
+                    function( response ) {
+                        console.info("cargando info:", response);
+                        console.info("#select-"+place);
+                            $.each(response.data, function( key, val ) {
+                                var option_ = $("<option/>");
+                                    option_.attr("value", val.nombre.value);
+                                    option_.attr("data-option", JSON.stringify(val));
+                                    option_.text(val.nombre.value);
+
+                                $("#select-"+place).append(option_);
+                             });
+                    });
+        }
