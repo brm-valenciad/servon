1c681bac5ccab4bca5b637fd6a4603aa6851e013
brm-valenciad <42819769+brm-valenciad@users.noreply.github.com>
2018-10-24 11:13:09 -0500

    Solo fechas desde hoy permitidas

--- infraestructura/logic.js
+++ infraestructura/logic.js
@@ -1,1 +1,10 @@
-console.info("Nos encargamos de hacer la logica");
+console.error("Nos encargamos de hacer la logica");
+
+    /*Formatear las fechas actuale*/
+        Date.prototype.toDateInputValue = (function() {
+            var local = new Date(this);
+            local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+            return local.toJSON().slice(0,10);
+        });
+        
+     $('#start-date').val(new Date().toDateInputValue()).attr("min", new Date().toDateInputValue());
