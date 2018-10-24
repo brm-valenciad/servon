e4bea019ed5274b92cc112d2ad4539e3d940f40f
brm-valenciad <42819769+brm-valenciad@users.noreply.github.com>
2018-10-24 11:47:52 -0500

    Refactorización de antigua logica

--- infraestructura/logic.js
+++ infraestructura/logic.js
@@ -43,3 +43,93 @@ console.warn("Formateamos las fechas");
                              });
                     });
         }
+
+//Desencadenar el eveto principal
+ $("body").on("change", "select#time-periods, input#amout-periods, #amount-jobs, select", function(){
+                globalCalculate();
+});
+
+function globalCalculate(){
+            var periodo     = ( $('#time-periods').val() == undefined || $('#time-periods').val() == "") ? "dia" : $('#time-periods').val();
+            var periodoText = ( periodo == "horas" ) ? "12_horas" : "tarifa_"+periodo;
+            var periodos = ( $('#amout-periods').val() == undefined || $('#amout-periods').val() == "") ? 0 : $('#amout-periods').val();
+            var cantidad = ( $('#amount-jobs').val() == undefined || $('#amount-jobs').val() == "" ) ? 0 : $('#amount-jobs').val(); 
+                
+            if ( isNaN(cantidad) == true || isNaN(periodos)  == true ){
+                alert("Existe un error con un valor no numerico");
+            }else{
+                //console.error("Falta change de valor unitario!");
+                var jobPlace          = $('#select-jobPlace').children('option:selected').data('option');
+                var computerEquipment = $('#select-computerEquipment').children('option:selected').data('option');
+                var computerLicences  = $('#select-computerLicences').children('option:selected').data('option');
+                var typeHeadbands     = $('#select-headbands').children('option:selected').data('option');
+                var lincencesMarkers  = $('#select-lincencesMarkers').children('option:selected').data('option');
+                
+                    //Puesto de trabajo
+                    if ( jobPlace != undefined ){
+                       obtainUnitValues( jobPlace, periodoText, periodos, cantidad, "jobPlace" );
+                    }else{ clearForm("jobPlace"); }
+
+                    //Equipo_de_computo
+                    if ( computerEquipment != undefined ){
+                       obtainUnitValues( computerEquipment, periodoText, periodos, cantidad, "computerEquipment" );
+                    }else{ clearForm("computerEquipment"); }
+
+                    //Lincencias
+                    if ( computerLicences != undefined ){
+                       obtainUnitValues( computerLicences, periodoText, periodos, cantidad, "computerLicences" );
+                    }else{  clearForm("computerLicences"); }
+
+                    //typeHeadbands
+                    if ( typeHeadbands != undefined ){
+                       obtainUnitValues( typeHeadbands, periodoText, periodos, cantidad, "headbands" );
+                    }else{  clearForm("headbands"); }
+
+                    //marcadoras
+                    if ( lincencesMarkers != undefined ){
+                       obtainUnitValues( lincencesMarkers, periodoText, periodos, cantidad, "lincencesMarkers" );
+                    }else{  clearForm("lincencesMarkers"); }
+
+                    $(".aditionals-elements").each(function(){
+                        var grupo = $(this).data('grupo');
+                        var optionDataAdic = $(this).children('option:selected').data('option');
+                            console.warn("grupo", grupo);
+                            console.info("optionDataAdic", optionDataAdic);
+                    }); 
+            }
+}
+
+       function obtainUnitValues(mainObj, searchBy, cantidad, periodos, identify){
+            console.info('#'+identify+'_unitario');
+            $('#'+identify+'_unitario').val( fNumber.go(mainObj[searchBy].value, "COP") );
+            $('#'+identify+'_total').val( fNumber.go(mainObj[searchBy].value*cantidad*periodos, "COP") );
+            $('#'+identify+'_total').data('cost', mainObj[searchBy].value * cantidad * periodos);     
+        }
+
+        var fNumber = {
+                sepMil: ".", /* separador para los miles*/
+                sepDec: ',', /* separador para los decimales*/
+                formatear:function (num){
+                    num +='';
+                    var splitStr = num.split('.');
+                    var splitLeft = splitStr[0];
+                    var splitRight = splitStr.length > 1 ? this.sepDec + splitStr[1] : '';
+                    var regx = /(\d+)(\d{3})/;
+                    while (regx.test(splitLeft)) {
+                        splitLeft = splitLeft.replace(regx, '$1' + this.sepMil + '$2');
+                    }
+                    return this.simbol+' '+ splitLeft + splitRight;
+                },
+                go:function(num, simbol){
+                    this.simbol = simbol ||'';
+                    return this.formatear(num);
+                }
+            }
+
+        function clearForm(identify){
+            $('#'+identify+'_unitario').val('');
+            $('#'+identify+'_unitario').val('');
+            $('#'+identify+'_unitario').data('cost','0');    
+        }
+
+
