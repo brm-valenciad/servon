caffb9cc6f6151e66aaa24324e9871c267836e61
brm-valenciad <42819769+brm-valenciad@users.noreply.github.com>
2018-11-06 15:58:01 -0500

    UPDATE

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -270,7 +270,7 @@ var number = 123456789;
 
 							//console.error( JstartDate.day() )
 
-							var businessDays = moment().recur(JstartDate, JendDate).every(daysToInclude);//.daysOfWeek();
+							var businessDays = moment().recur(JstartDate, JendDate).every(daysToInclude).daysOfWeek();
 							var totalDaysGestion  = businessDays.all().length,
 								diffHours  = Math.abs(JstartDate.diff(JendDate, 'hours')),
 								diffWeeks  = Math.abs(JstartDate.diff(JendDate, 'weeks'));
