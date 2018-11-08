1cd0ba54d9b601de22df3cd128ab2e6d3fbfbb39
brm-valenciad <42819769+brm-valenciad@users.noreply.github.com>
2018-11-08 11:11:33 -0500

    UPDATE

--- pagos/payments.js
+++ pagos/payments.js
@@ -59,8 +59,8 @@ function InsertOmoldsIfraestructura(tablOM){
                         $(".pay").append(input_);
                     }; 
                    
-                    $(".pay").submit();
+                    //$(".pay").submit();
                 //}
         }); 
 
-console.warn("Haciendo los pagos 2");
+console.warn("Haciendo los pagos 3");
