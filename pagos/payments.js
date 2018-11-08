e5050db6e491aac5c9b831c0b5f9e9f6e606f908
brm-valenciad <42819769+brm-valenciad@users.noreply.github.com>
2018-11-08 14:47:47 -0500

    UPDATE

--- pagos/payments.js
+++ pagos/payments.js
@@ -8,7 +8,7 @@ var payUdata = [
 		    	{ name: "taxReturnBase", value:"0" },
 		    	{ name: "currency", value:"COP" },
 		   		{ name: "test", value:"0" },
-		   		{ name: "buyerEmail", value:"duvan@prueba.com" }
+		   		{ name: "buyerEmail", value:"duvan@prueba.com" },
 		   		{ name: "responseUrl", value:"https://www.servon.com.co/web/index/response/" },
 		   		{ name: "confirmationUrl", value:"https://www.servon.com.co/web/index/returnpayment/" }
 			];
@@ -103,4 +103,4 @@ $('#saveInBD').submit(function( event ) {
                 }
         }); 
 
-console.warn("Haciendo los pagos 21");
+console.warn("Haciendo los pagos 22");
